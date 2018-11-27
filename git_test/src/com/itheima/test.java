021943601bfc94d6444ece2766b9fa7460d5dfbd
Ethan <Ethan@itheima.com>
2018-11-27 15:42:20 +0800

    第一次添加math()

--- git_test/src/com/itheima/test.java
+++ git_test/src/com/itheima/test.java
@@ -4,4 +4,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello Git !");
     }
+    public void math(){
+        int i = 0;
+        i +=10;
+    }
 }
